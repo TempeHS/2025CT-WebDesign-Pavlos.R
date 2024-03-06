e6df8a5389c5d249cba50f3ff20c25af99f3323a
PavlosR <159206339+PavlosR@users.noreply.github.com>
2024-03-07 10:33:34 +1100

    My First Javascript

---
+++ script.js
@@ -0,0 +1,12 @@
+let MyPrompt = prompt("First Number");
+let InsertName = prompt("Second Number");
+
+if (InsertName == 0)
+{
+    alert("Error");
+}
+
+if (InsertName != 0)
+{
+    alert(MyPrompt / InsertName);
+}
